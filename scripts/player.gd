43418b2c7c315f6246ba2ca025ec3e9e18cb8c30
toger5 <toger5@hotmail.de>
2016-04-06 16:25:52 +0200

    improved player control (it's now possible to press w and a and still have the same speed but another direction)

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,36 +2,34 @@
 
 extends RigidBody
 
-var colliderClass = load("scripts/collider.gd")
+
 export var sensitivity = 1.0
 export var speed = 0.2
 export var boost = 2
 export var Jump_Speed = 1.0
-onready var collider = colliderClass.new(10,get_pos_int(), self, get_node("/root/Node/PlayerCollision"))
+var b
 var active = true
 var in_air = false
 var cam
 var lastpos
 var lastChunk = Vector2(0,0)
-var destroy_oneBlock = false
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	cam = get_node("Camera")
 	set_fixed_process(true)
 	Input.warp_mouse_pos(get_viewport().get_rect().size * 0.5)
 	lastChunk = getCurrentChunk() + Vector2(1,0)
-	get_node("Camera/RayClick").add_exception(self)
+	
 func getCurrentChunk():
 	var intpos = Vector2(int(get_translation().x),int(get_translation().z))
 	return Vector2(floor(intpos.x/16.0)*16, floor(intpos.y/16.0)*16)
 
-#func createInitialPhysic():
-#	var chunks = get_node("/root/Node").chunk_dict
-#	for x in [-16,0,16]:
-#		for y in [-16,0,16]:
-#			if Vector2(getCurrentChunk().x+x, getCurrentChunk().y+y) in chunks:
-#				chunks[Vector2(getCurrentChunk().x+x, getCurrentChunk().y+y) ].activate_physic()
+func createInitialPhysic():
+	var chunks = get_node("/root/Node").chunk_dict
+	for x in [-16,0,16]:
+		for y in [-16,0,16]:
+			if Vector2(getCurrentChunk().x+x, getCurrentChunk().y+y) in chunks:
+				chunks[Vector2(getCurrentChunk().x+x, getCurrentChunk().y+y) ].activate_physic()
 	
 func _fixed_process(delta):
 	#pysic activieren im richtigen bereich
@@ -43,33 +41,32 @@ func _fixed_process(delta):
 		lastChunk = currentChunk
 		print("direction: ",dir)
 		#refresh physic
-#		for felder in [-16,0,16]:
-#			var cActivate = Vector2(0,0) + currentChunk #             felder*dir.y, fleder*dir.x
-#			cActivate.x = felder*dir.y + dir.x*16 + currentChunk.x
-#			cActivate.y = felder*dir.x + dir.y*16 + currentChunk.y 
-#			if cActivate in chunks:
-#				chunks[cActivate].activate_physic()
-#			var cDeactivate = Vector2(0,0)#x + (-dir.x * 16), 2*y*-dir.y) + currentChunk
-#			cDeactivate.x = felder*dir.y - dir.x * 32 + currentChunk.x
-#			cDeactivate.y = felder*dir.x - dir.y * 32 + currentChunk.y 
-#			if cDeactivate in chunks:
-#				print("deactivate physic for: ",cDeactivate)
-#				chunks[cDeactivate].ddeactivate_physic()
-#				
+		for felder in [-16,0,16]:
+			var cActivate = Vector2(0,0) + currentChunk #             felder*dir.y, fleder*dir.x
+			cActivate.x = felder*dir.y + dir.x*16 + currentChunk.x
+			cActivate.y = felder*dir.x + dir.y*16 + currentChunk.y 
+			if cActivate in chunks:
+				chunks[cActivate].activate_physic()
+			var cDeactivate = Vector2(0,0)#x + (-dir.x * 16), 2*y*-dir.y) + currentChunk
+			cDeactivate.x = felder*dir.y - dir.x * 32 + currentChunk.x
+			cDeactivate.y = felder*dir.x - dir.y * 32 + currentChunk.y 
+			if cDeactivate in chunks:
+				print("deactivate physic for: ",cDeactivate)
+				chunks[cDeactivate].deactivate_physic()
+				
 				
 		#call function for nwe generation ring
-		get_node("/root/Node").entered_new_chunk(dir,currentChunk)
+		get_parent().entered_new_chunk(dir,currentChunk)
 #	var chunkx = floor(intpos.x/16.0)*16
 #	var chunky = floor(intpos.y/16.0)*16
 	
 
 	
 	
-	if lastpos != get_pos_int():
-		#print("newPos: ",get_pos_int())
-		lastpos = get_pos_int()
-		collider.refresh(lastpos)
-		
+	if lastpos != getPosInt():
+		#print("newPos: ",getPosInt())
+		lastpos = getPosInt()
+		#get_node("/root/Node/world").refresh_collider()
 	if Input.is_key_pressed(KEY_O) and active == false:
 		active = true
 		print(active)
@@ -78,10 +75,6 @@ func _fixed_process(delta):
 		print(active)
 	if Input.is_action_pressed("jump"):
 		jumpen()
-	if Input.is_action_pressed("l_click") and not destroy_oneBlock:
-		destroyClickedBlock()
-	if not Input.is_action_pressed("l_click"):
-		destroy_oneBlock = false
 		#in_air = true
 
 	#steuerung um sich mit der maus umzusehen
@@ -104,7 +97,7 @@ func _fixed_process(delta):
 		#move(KEY_Q,get_transform().basis.xform(Vector3(0,-1,0)))
 		#move(KEY_E,get_transform().basis.xform(Vector3(0,1,0)))
 func move(k,dir):
-	var b = 1
+	b = 1
 	if(Input.is_key_pressed(k)):
 		if(Input.is_key_pressed(KEY_SHIFT)):
 			b = boost
@@ -116,30 +109,19 @@ func move(k,dir):
 		#set_linear_velocity(dir * speed * 50)
 
 func jumpen():
-	var curV = get_linear_velocity()
-	set_linear_velocity(Vector3(curV.x, Jump_Speed, curV.z))
+	#print("jo")
 	if not in_air:
 		var curV = get_linear_velocity()
 		set_linear_velocity(Vector3(curV.x, Jump_Speed, curV.z))
 		in_air = true
 	else:
-		var ray_cast = get_node("RayCast")
+		var ray_cast = get_node("/root/Node/Player/RayCast")
 		ray_cast.set_enabled(true)
+		
 		if ray_cast.is_colliding():
 			in_air = false
-		ray_cast.set_enabled(false)
-		
-func destroyClickedBlock():
-	print(destroy_oneBlock)
-	var ray = get_node("Camera/RayClick")
-	#var nor = ray.get_collision_normal()
-	print(ray.get_collider())
-	if ray.get_collider() != null:
-		var pos = ray.get_collider().get_translation()
-		
-		utils.get_chunc_by_coord(get_node("/root/Node").chunk_dict,pos).remove_block(pos)
-		destroy_oneBlock = true
-	
-func get_pos_int():
+			ray_cast.set_enabled(false)
+
+func getPosInt():
 	var g = get_translation()
 	return Vector3(round(g.x),round(g.y),round(g.z))
